234110826b0815032d03b053a16cfe64b8b38aea
A-dash <aadarsmain101@gmail.com>
2021-07-07 21:05:10 +0545

    'done_upto_mcq_section_but_without_mcq_mobile_view'

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// const t = document.getElementsByClassName('btn-secondary')
+// t.addEventListener('click',function(){
+//        console.log('Clickk')
+// })
+// if (tog){
+//     tog.addEventListener('click',function(){
+//         console.log('Clickk')
+//     })
+// }else{
+//     console.log('Not found')
+// }
+
+
+
+
+
+
+
+
+
+
+
