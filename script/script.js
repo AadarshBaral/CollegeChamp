abb6fb22b1e2bfe1f21c7a4e36a631991c4fd9cb
A-dash <aadarsmain101@gmail.com>
2021-07-09 12:57:19 +0545

    'implemented_responsive_navbar'

---
+++ script/script.js
@@ -0,0 +1,30 @@
+
+const toggleButton = document.getElementsByClassName('toggle-btn')[0]
+const toggleButton1 = document.getElementsByClassName('toggle-btn1')[0]
+
+const navbarLinks = document.getElementsByClassName('nav-links')[0]
+
+
+toggleButton.addEventListener('click', () => {
+  console.log('click')
+  navbarLinks.classList.toggle('active')
+  toggleButton.classList.toggle('hide')
+  toggleButton1.classList.toggle('act')
+})
+toggleButton1.addEventListener('click', () => {
+  console.log('click')
+  navbarLinks.classList.toggle('active')
+  toggleButton1.classList.toggle('act')
+  toggleButton.classList.toggle('hide')
+
+})
+
+
+
+
+
+
+
+
+
+
