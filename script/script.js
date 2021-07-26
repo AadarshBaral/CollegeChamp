915ed10a5af7414d56a768bbc5241761fbc10ae5
A-dash <aadarsmain101@gmail.com>
2021-07-26 14:36:11 +0545

    redesigned blog cards

--- script/script.js
+++ script/script.js
@@ -3,16 +3,24 @@ const toggleButton = document.getElementsByClassName('toggle-btn')[0]
 const toggleButton1 = document.getElementsByClassName('toggle-btn1')[0]
 
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
+// const dropdown = document.getElementsByClassName('dropdown')[0]
+// const subclass_items = document.querySelectorAll('.subclass-items')[0]
 
+
+// dropdown.addEventListener('click',() =>{
+// subclass_items.style.display = 'flex'
+// console.log('clixkk')
+
+// })
 
 toggleButton.addEventListener('click', () => {
-  console.log('click')
+ 
   navbarLinks.classList.toggle('active')
   toggleButton.classList.toggle('hide')
   toggleButton1.classList.toggle('act')
 })
 toggleButton1.addEventListener('click', () => {
-  console.log('click')
+ 
   navbarLinks.classList.toggle('active')
   toggleButton1.classList.toggle('act')
   toggleButton.classList.toggle('hide')
@@ -30,7 +38,20 @@ document.querySelector("iframe").addEventListener("load",
 
 
 
+var btn2 = document.getElementById('btn2');
 
 
-
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block') {
+    e.style.display = 'none';
+  }
+  else {
+    e.style.display = 'block';
+    
+  }
+  
+  console.log('clicked');
+  return;
+}
 
